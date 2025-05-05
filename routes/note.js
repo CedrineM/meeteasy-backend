2373c122b87ed5cf4b1ad1b611337f4ecd2386be
CedrineM <cedrinemilesi@gmail.com>
2025-05-05 16:04:19 +0200

    Feat: finalisation du CRUD des notes
    - Ajout de la route PUT /note/:id pour modifier une note
    - Ajout de la route DELETE /note/:id avec vérification d’autorisation
    - Suppression de la note du tableau user.notes après suppression

--- routes/note.js
+++ routes/note.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 //import des modèles
 const Note = require("../models/Note");
@@ -71,6 +72,12 @@ router.get("/note/:id", async (req, res) => {
     //récupération de la note
     const note = await Note.findById(req.params.id);
     console.log("noteFind", note);
+    if (!note) {
+      throw {
+        message: "Note not found",
+        status: 404,
+      };
+    }
 
     //vérifier autorisation de lecture
     const user = await User.findOne({
@@ -88,7 +95,7 @@ router.get("/note/:id", async (req, res) => {
     if (user._id !== note.userId) {
     } else {
       throw {
-        message: "You not autorize to read this note",
+        message: "You not autorize to modify this note",
         status: 401,
       };
     }
@@ -118,6 +125,121 @@ router.get("/notes", async (req, res) => {
   }
 });
 //Update
-//Delete
+router.put("/note/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+    //récupération de la note
+    const note = await Note.findById(req.params.id);
+    console.log("noteFind", note);
 
+    if (!note) {
+      throw {
+        message: "Note not found",
+        status: 404,
+      };
+    }
+    //vérifier autorisation de lecture
+    const user = await User.findOne({
+      token: req.headers.authorization.replace("Bearer ", ""),
+    }).populate("notes");
+
+    if (!user) {
+      throw {
+        message: "This user not existe",
+        status: 401,
+      };
+    }
+    // console.log("tabNotes", user.notes);
+
+    if (user._id !== note.userId) {
+    } else {
+      throw {
+        message: "You not autorize to read this note",
+        status: 401,
+      };
+    }
+
+    //mise à jour de la note via Object.assign
+
+    const {
+      meetingType,
+      meetingDate,
+      meetingObject,
+      participants,
+      noteContent,
+    } = req.body;
+
+    Object.assign(note, {
+      meetingType,
+      meetingDate,
+      meetingObject,
+      participants,
+      noteContent,
+    });
+
+    await note.save();
+    return res.status(200).json(note);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(error.status || 500)
+      .json({ message: error.message || "Internal Server Error" });
+  }
+});
+
+//Delete --> pas encore de middleware
+router.delete("/note/:id", async (req, res) => {
+  try {
+    // quand middleware et vérification d'autorisation utiliser seulement findone and delete
+
+    console.log(req.params.id);
+    //récupération de la note
+    const note = await Note.findById(req.params.id);
+    console.log("noteFind", note);
+    if (!note) {
+      throw {
+        message: "Note not found",
+        status: 404,
+      };
+    }
+
+    //vérifier autorisation de lecture
+    const user = await User.findOne({
+      token: req.headers.authorization.replace("Bearer ", ""),
+    }).populate("notes");
+
+    if (!user) {
+      throw {
+        message: "This user not existe",
+        status: 401,
+      };
+    }
+    // console.log("tabNotes", user.notes);
+
+    if (!user._id.equals(note.userId)) {
+      throw {
+        message: "You are not authorized to delete this note",
+        status: 401,
+      };
+    }
+
+    // Suppression de la note
+    const result = await Note.deleteOne({ _id: req.params.id });
+
+    if (result.deletedCount === 0) {
+      throw { message: "Delete failed", status: 400 };
+    }
+
+    await user.updateOne({
+      $pull: { notes: new mongoose.Types.ObjectId(req.params.id) },
+    });
+
+    return res.status(200).json({ message: "Note successfully deleted" });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(error.status || 500)
+      .json({ message: error.message || "Internal Server Error" });
+  }
+});
 module.exports = router;
